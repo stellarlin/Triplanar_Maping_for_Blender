95ae44f88b39a89616c28dfd4e710c03d9caa2e4
Sofiia Prykhach <prykhsof@cvut.cz>
2024-12-29 15:23:49 +0100

    Add-on description changes

--- project_files/__init__.py
+++ project_files/__init__.py
@@ -1,27 +1,14 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 
 bl_info = {
-    "name": "Triplane Texture Mapping",
+    "name": "Triplanar Texture Mapping",
     "author": "Sofiia Prykhach",
     "version": (1, 0, 0),
-    "blender": (4, 2, 0),
-    "location": "View3D > Tool Shelf",
-    "description": "",
+    "blender": (4, 3, 0),
+    "location": "Properties > Material Tab > Triplanar Mapping",
+    "description": "Adds triplanar texture mapping functionality to Blender, enabling seamless texturing without UV maps.",
     "warning": "",
     "doc_url": "",
-    "category": "",
+    "category": "Material",
 }
 
 from .blender_classes import triplanar_properties as properties
